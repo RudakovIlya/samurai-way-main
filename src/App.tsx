bda7d7cf496e53127bf435446119446af9087a0e
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-27 11:29:14 +0300

    watch and complete video tutorial No. 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <div>first div in App</div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+                <li>ts</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
