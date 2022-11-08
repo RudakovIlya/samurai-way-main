9ab5f84308416414375064b3cd6ed341bb717ab4
ilya <rudakov.alhan@gmail.com>
2022-11-08 17:23:58 +0300

    watch and complete video tutorial No. 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from "./components/Technologies/Technologies";
-import {Header} from "./components/Header/Header";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-           <Technologies/>
+        <div className={'App'}>
+            <ul>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
         </div>
     );
 }
