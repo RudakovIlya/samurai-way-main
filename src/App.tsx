523664353a823dcaecdbd54e0548b79a3811ceea
ilya <rudakov.alhan@gmail.com>
2022-11-08 20:56:18 +0300

    added styles for starting layout and watch video tutorial № 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img src='https://cdn-icons-png.flaticon.com/512/179/179337.png' alt={'compass'}/>
+            </header>
+            <nav className={'nav'}>
+                <ul>
+                    <li><a href={'#s'}>Profile</a></li>
+                    <li><a href={'#s'}>Messages</a></li>
+                    <li><a href={'#s'}>News</a></li>
+                    <li><a href={'#s'}>Music</a></li>
+                    <li><a href={'#s'}>Settings</a></li>
+                </ul>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img src={"https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg"} alt={'content'}/>
+                </div>
+                <div>ava + description</div>
+                <ul>
+                    my posts
+                    <li>new post 1</li>
+                    <li>new post 2</li>
+                    <li>new post 3</li>
+                </ul>
+            </div>
         </div>
     );
 };
