3e6d87e4abc8d83acfff17c3c9434fb8126194e0
ilya <rudakov.alhan@gmail.com>
2022-11-09 15:59:31 +0300

    add icons for navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import classes from './Navbar.module.css';
+import icon from './hooli-logo.svg'
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <ul className={classes.list}>
                 <li className={`${classes.item} ${classes.active}`}><a href={'#s'}>Profile</a></li>
-                <li className={classes.item}><a href={'#s'}>Messages</a></li>
-                <li className={classes.item}><a href={'#s'}>News</a></li>
-                <li className={classes.item}><a href={'#s'}>Music</a></li>
-                <li className={classes.item}><a href={'#s'}>Settings</a></li>
+                <li className={classes.item}>
+                    <img src={icon} alt="message"/>
+                    <a href={'#s'}>Messages</a>
+                </li>
+                <li className={classes.item}>
+                    <img src={icon} alt="message"/>
+                    <a href={'#s'}>News</a>
+                </li>
+                <li className={classes.item}>
+                    <img src={icon} alt="message"/>
+                    <a href={'#s'}>Music</a>
+                </li>
+                <li className={classes.item}>
+                    <img src={icon} alt="message"/>
+                    <a href={'#s'}>Settings</a>
+                </li>
             </ul>
         </nav>
     );
