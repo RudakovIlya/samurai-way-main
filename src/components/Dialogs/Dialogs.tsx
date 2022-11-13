58cdf2f334e9e2ac8d9541b5dcb009e4ebf4a6a8
ilya <rudakov.alhan@gmail.com>
2022-11-13 17:47:55 +0300

    feat: added layout for dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import styles from './Dialogs.module.scss'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={styles.dialogsContainer}>
+            <div className={`${styles.Dialogs} ${styles.column}`}>
+                <ul className={`${styles.listDialogs} ${styles.list}`}>
+                    <li className={styles.dialog}>
+                        <NavLink to={'/dialogs/1'}>Dimych</NavLink>
+                    </li>
+                    <li className={styles.dialog}>
+                        <NavLink to={'/dialogs/2'}>Dimych</NavLink>
+                    </li>
+                    <li className={styles.dialog}>
+                        <NavLink to={'/dialogs/3'}>Dimych</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className={`${styles.Messages} ${styles.column}`}>
+                <ul className={`${styles.listMessages} ${styles.list}`}>
+                    <li className={styles.message}>Hello!</li>
+                    <li className={styles.message}>Hello!</li>
+                    <li className={styles.message}>Hello!</li>
+                </ul>
+            </div>
+        </div>
     )
 };
