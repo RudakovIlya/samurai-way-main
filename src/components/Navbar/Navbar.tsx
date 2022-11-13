89db6d73e798db45b4ff887dba0486daf604b058
ilya <rudakov.alhan@gmail.com>
2022-11-13 16:39:58 +0300

    feat: added media-quires for App css component and NavBar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from './Navbar.module.scss';
-import settings from './icons/settings_16.svg'
-import home from './icons/home.svg'
-import music from './icons/music.svg'
-import mes from './icons/message.svg'
-import news from './icons/news.svg'
+import settings from './icons/settings_16.svg';
+import home from './icons/home.svg';
+import music from './icons/music.svg';
+import mes from './icons/message.svg';
+import news from './icons/news.svg';
 import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
@@ -21,15 +21,15 @@ const Navbar = () => {
                 </li>
                 <li className={styles.item}>
                     <img src={news} alt="message"/>
-                    <NavLink to={'/news'}>News</NavLink>
+                    <NavLink to={'/news'} activeClassName={styles.activeLink}>News</NavLink>
                 </li>
                 <li className={styles.item}>
                     <img src={music} alt="music"/>
-                    <NavLink to={'/music'}>Music</NavLink>
+                    <NavLink to={'/music'} activeClassName={styles.activeLink}>Music</NavLink>
                 </li>
                 <li className={`${styles.item} ${styles.rotate}`}>
                     <img src={settings} alt="settings"/>
-                    <NavLink to={'/settings'}>Settings</NavLink>
+                    <NavLink to={'/settings'} activeClassName={styles.activeLink}>Settings</NavLink>
                 </li>
             </ul>
         </nav>
