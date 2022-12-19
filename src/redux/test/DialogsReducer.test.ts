a0ad02dc7c74df508cd6c4e7e9acab263d813741
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2022-12-19 15:29:39 +0300

    feat: added simplest test for DialogsReducer

---
+++ src/redux/test/DialogsReducer.test.ts
@@ -0,0 +1,120 @@
+import {v1} from "uuid";
+import {addNewMessage, DialogReducer, updateNewMessageTextAC} from "../DialogReducer";
+import {DialogPageType} from "../store";
+
+test('new message should be added to the dialog', () => {
+
+    const startState: DialogPageType = {
+        dialogs: [
+            {
+                id: v1(),
+                name: 'Ilych',
+                avatar: 'https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg'
+            },
+            {
+                id: v1(),
+                name: 'Ilych',
+                avatar: 'https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg'
+            },
+            {
+                id: v1(),
+                name: 'Ilych',
+                avatar: 'https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg'
+            },
+            {
+                id: v1(),
+                name: 'Ilych',
+                avatar: 'https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg'
+            },
+        ],
+        messages: [
+            {
+                id: v1(),
+                text: 'Ilych',
+                avatar: '"https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg"'
+            },
+            {
+                id: v1(),
+                text: 'Ilych',
+                avatar: '"https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg"'
+            },
+            {
+                id: v1(),
+                text: 'Ilych',
+                avatar: '"https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg"'
+            },
+            {
+                id: v1(),
+                text: 'Ilych',
+                avatar: '"https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg"'
+            },
+        ],
+        newMessageText: ''
+    }
+
+    const newMessageText = 'New Message Text'
+
+    const endState = DialogReducer(startState, addNewMessage(newMessageText));
+
+    expect(endState.messages[endState.messages.length - 1].text).toBe(newMessageText)
+    expect(endState.messages.length).toBe(startState.messages.length + 1);
+    expect(endState.dialogs).toStrictEqual(startState.dialogs)
+})
+
+test('correct message text value should be overwritten', () => {
+
+    const startState: DialogPageType = {
+        dialogs: [
+            {
+                id: v1(),
+                name: 'Ilych',
+                avatar: 'https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg'
+            },
+            {
+                id: v1(),
+                name: 'Ilych',
+                avatar: 'https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg'
+            },
+            {
+                id: v1(),
+                name: 'Ilych',
+                avatar: 'https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg'
+            },
+            {
+                id: v1(),
+                name: 'Ilych',
+                avatar: 'https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg'
+            },
+        ],
+        messages: [
+            {
+                id: v1(),
+                text: 'Ilych',
+                avatar: '"https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg"'
+            },
+            {
+                id: v1(),
+                text: 'Ilych',
+                avatar: '"https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg"'
+            },
+            {
+                id: v1(),
+                text: 'Ilych',
+                avatar: '"https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg"'
+            },
+            {
+                id: v1(),
+                text: 'Ilych',
+                avatar: '"https://img01.rl0.ru/afisha/e1500x600i/daily.afisha.ru/uploads/images/d/35/d35d7e33e07f4bcbaa1b68379a467263.jpg"'
+            },
+        ],
+        newMessageText: ''
+    }
+
+    const newMessageText = 'New Message Text'
+
+    const endState = DialogReducer(startState, updateNewMessageTextAC(newMessageText));
+
+    expect(endState.newMessageText).toBe(newMessageText)
+
+})
